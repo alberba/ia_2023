ad6d8759bf8a04e1b7eba5ac85e740258f174df3
alberba <83814585+alberba@users.noreply.github.com>
2023-10-10 14:11:50 +0200

    Actualizacion

--- monedes/agent.py
+++ monedes/agent.py
@@ -25,3 +25,25 @@ class AgentMoneda(agent.Agent):
         self, percepcio: entorn.Percepcio
     ) -> entorn.Accio | tuple[entorn.Accio, object]:
         pass
+
+class Estat:
+    def __init__(self, accions_previes = None) -> None:
+        if accions_previes is None:
+            accions_previes = []
+
+        self.monedas = []
+        self.accions_previes = accions_previes
+    
+    def genera_fills(self) -> list:
+        posEspai = self.monedas.index(" ")
+        self.genera_fills_botar(posEspai)
+
+    def genera_fills_botar(self, posEspai) -> list:
+        for i in range(2):
+            accio = self.monedas
+            if posEspai >= 2:
+                accio[posEspai],  accio[posEspai - 2] =  accio[posEspai - 2], accio[posEspai]
+                if accio[posEspai] == "X":
+                    accio[posEspai] = "C"
+                elif accio[posEspai] == "C":
+                    accio[posEspai] = "X"
