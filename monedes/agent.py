875b4a6887e44f118c730144d0d4938b23bcfb38
alberba <albertsalomv@gmail.com>
2023-10-16 23:17:28 +0200

    Posible solucion con fallos

--- monedes/agent.py
+++ monedes/agent.py
@@ -5,6 +5,7 @@ Percepcions:
 Solució:
     " XXXC"
 """
+import copy
 
 from ia_2022 import agent, entorn
 
@@ -27,23 +28,77 @@ class AgentMoneda(agent.Agent):
         pass
 
 class Estat:
-    def __init__(self, accions_previes = None) -> None:
+    def __init__(self, monedas: str, pare = None, accions_previes = None) -> None:
         if accions_previes is None:
             accions_previes = []
 
-        self.monedas = []
+        self.monedas = monedas
+        self.pare = pare
         self.accions_previes = accions_previes
+        self.accions_pos = []
+        self.coste = 0
+        self.heuristica = None
+
+    def es_meta(self) -> bool:
+        return self.monedas == list(SOLUCIO)
     
     def genera_fills(self) -> list:
         posEspai = self.monedas.index(" ")
-        self.genera_fills_botar(posEspai)
+        self.accions_pos += self.genera_fills_botar(posEspai)
+        self.accions_pos += self.genera_fills_desplaçar(posEspai)
+        self.accions_pos += self.generar_fills_girar()
 
-    def genera_fills_botar(self, posEspai) -> list:
-        for i in range(2):
-            accio = self.monedas
-            if posEspai >= 2:
-                accio[posEspai],  accio[posEspai - 2] =  accio[posEspai - 2], accio[posEspai]
-                if accio[posEspai] == "X":
-                    accio[posEspai] = "C"
-                elif accio[posEspai] == "C":
-                    accio[posEspai] = "X"
+        estats_generats = []
+
+        for accio, coste in self.accions_pos:
+            nou_estat = copy.deepcopy(self)
+            nou_estat.pare = (self)
+            nou_estat.accions_previes.append(accio)
+
+            self.monedas = accio
+            self.coste += coste
+            self.heuristica = self.calcular_heuristica(accio.index(" "))
+            
+            estats_generats.append(nou_estat)
+
+        return estats_generats
+
+    def genera_fills_botar(self, posEspai) -> list[str, int]:
+        poss_accions = []
+        for offset in [-2, 2]:
+            if 0 <= posEspai + offset < len(self.monedas):
+                accio = self.monedas.copy()
+                accio[posEspai], accio[posEspai + offset] = accio[posEspai + offset], accio[posEspai]
+                accio[posEspai] = girar_moneda(accio[posEspai])
+                poss_accions.append([accio, 3])
+        return poss_accions
+            
+    def genera_fills_desplaçar(self, posEspai) -> list[str, int]:
+        pos_accions = []
+        for offset in [-1, 1]:
+            if 0 <= posEspai + offset < len(self.monedas):
+                accio = self.monedas.copy()
+                accio[posEspai], accio[posEspai + offset] = accio[posEspai + offset], accio[posEspai]
+                pos_accions.append([accio, 1])
+        return pos_accions
+    
+    def generar_fills_girar(self) -> list[str, int]:
+        pos_accions = []
+        for i in range(len(self.monedas)):
+            accio = self.monedas.copy()
+            if not accio[i] == " ":
+                accio[i] = girar_moneda(accio[i])
+                pos_accions.append([accio, 2])
+        return pos_accions
+    
+    def calcular_heuristica(self, pos_espacio) -> int:
+        vx = sum(1 for i in range(1, len(SOLUCIO)) if self.monedas[i] == SOLUCIO[i])
+        return vx + pos_espacio
+
+def girar_moneda(moneda):
+    if moneda == "X":
+        moneda = "C"
+    elif moneda == "C":
+        moneda = "X"
+    return moneda
+
